ed00ebe2cf81642da7c01dc27e079e86133b31db
yuhoyeon <zz31@naver.com>
2020-03-05 21:57:28 +0900

    Create Follower.cpp

--- Follower/src/Follower.cpp
+++ Follower/src/Follower.cpp
@@ -1,51 +1,71 @@
 #include "ros/ros.h"
 #include "std_msgs/Float32.h"
-
 #include "openpose_ros_msgs/BoundingBox.h"
 #include "openpose_ros_msgs/OpenPoseHuman.h"
 #include "openpose_ros_msgs/OpenPoseHumanList.h"
 #include "openpose_ros_msgs/PointWithProb.h"
 
-float Dist = 0.0;
-bool Data_receving = false;
+/***************************************************
+ *  Class SpdCtrl
+ ***************************************************/
+class SpdCtrl{
+private:
+    float Dist = 0.0;
+    bool Data_receving = false;
+    ros::Subscriber Follow;
+    ros::Publisher Spd_Robot;
+    std_msgs::Float32 spd;
+public:
+    SpdCtrl(ros::NodeHandle* nh);
+    ~SpdCtrl(){}
+    void Callback(const openpose_ros_msgs::OpenPoseHumanList::ConstPtr& msg);
+    float speedOfRobot(const float d);
+    void PubSpd();
+};
 
-void Callback(const std_msgs::Float32& msg);
-float speedOfRobot(const float d);
+//Class Method Define
+SpdCtrl::SpdCtrl(ros::NodeHandle* nh){
+    Follow  = nh -> subscribe("distance_msg", 1, &SpdCtrl::Callback, this);
+    Spd_Robot = nh -> advertise<std_msgs::Float32>("SPD", 1);
+    Dist = 0.0;
+    Data_receving = false;
+    ros::Rate loop_rate(10);
+    ROS_INFO("Follower Node in Online");
+}
+void SpdCtrl::Callback(const openpose_ros_msgs::OpenPoseHumanList::ConstPtr& msg){
+    if (msg->num_humans == 0 ){
+        Data_receving = false;
+        return;
+    }
+    Dist = msg->human_list[0].body_key_points_with_prob[10].x;
+    Data_receving = true;
+}
+float SpdCtrl::speedOfRobot(const float d){
+    float speed;
+    if (d < 0) 
+        return 0;
+    speed = d * 2;
+    return speed;
+}
+void SpdCtrl::PubSpd(){
+    if(Data_receving == true){
+        spd.data = speedOfRobot(Dist);
+        Spd_Robot.publish(spd);
+        Data_receving = false;
+    }
+}
+//**************************************************
+// END Class SpdCtrl
+//================================================
 
 int main(int argc, char **argv){
     ros::init(argc, argv, "Follower");
     ros::NodeHandle nh;
-
-    ros::Subscriber Follow = nh.subscribe("distance_msg", 1, Callback);
-    ros::Publisher Spd_Robot = nh.advertise<std_msgs::Float32>("SPD", 1);
-    ros::Rate loop_rate(10);
-    std_msgs::Float32 spd;
-
-    ROS_INFO("Follower Node in Online");
-
+    SpdCtrl Spdcontrol(&nh);
     while (ros::ok()){
-        ros::spinOnce();        
-        if(Data_receving == true){
-            spd.data = speedOfRobot(Dist);
-            Spd_Robot.publish(spd);
-            Data_receving = false;
-        }
+        ros::spinOnce();    
+        Spdcontrol.PubSpd();    
     }
     ROS_INFO("Follower Node OFF");
     return 0;
-}
-
-void Callback(const std_msgs::Float32& msg){
-    Dist = msg.data;
-    Data_receving = true;
-}
-
-float speedOfRobot(const float d){
-    float speed;
-    if (d < 0) 
-        return 0;
-
-    speed = d * 2;
-
-    return speed;
 }
